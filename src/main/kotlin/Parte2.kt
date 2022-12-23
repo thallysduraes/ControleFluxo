2e68cb85f4b73d2890537be29794d828b223c96c
Thallys Durães <thallysduraes@hotmail.com>
2022-12-23 22:41:29 +0000

    Complemento dos exercicios

--- src/main/kotlin/Parte2.kt
+++ src/main/kotlin/Parte2.kt
@@ -3,7 +3,7 @@ https://github.com/DevMasterTeam/Udemy-Andorid-Kotlin/blob/master/Secao%2007%20-
 */
 
 fun main() {
-    ex8(5)
+    desafio()
 }
 
 //Exercícios fluxo - For
@@ -42,10 +42,63 @@ fun ex8(n: Int) {
 
     for (i in 1..n) {
         qtd = i;
-        for (j in i..qtd){
+        for (j in i..qtd) {
             print("#")
         }
     }
 }
 
+//Exercícios fluxo - While
 
+fun ex9() {
+    val caixa = 2000
+    val capacidade = 7
+    var numBaloes = 0
+
+    while (numBaloes * capacidade + capacidade < caixa) {
+        numBaloes++
+    }
+    println(numBaloes)
+}
+
+fun ex10() {
+    var i = 1
+    while (i <= 50) {
+        if (1 % 15 == 0) {
+            print("FizzBuzz ")
+        } else if (i % 3 == 0) {
+            print("Buzz ")
+        } else if (i % 5 == 0) {
+            print("Fizz ")
+        } else {
+            print("$i ")
+        }
+        i++
+    }
+}
+
+fun ex11(str: String) {
+    var i = str.length - 1
+    while (i >= 0){
+        print(str[i])
+        i--
+    }
+}
+
+fun desafio(){
+    val salario = 10000f
+    var ana = 0f
+    var paula = 0f
+    var mes = 0
+
+    do{
+        ana += (salario * 0.05f) + (salario * 0.05f) + (ana * 0.002f);
+        paula += (salario * 0.05f) + (paula * 0.008f);
+        mes++;
+        println("Patrimônio Ana: R$$ana -" +
+                " Patrimônio Paula: R$$paula");
+    }while (ana > paula)
+    println("Ana - R$${(ana * 0.002f)}")
+    println("Paula - R$${(paula * 0.008f)}")
+    println("Paula irá ter o patrimônio de Ana no mês $mes")
+}
